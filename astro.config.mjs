a14414140f04d94ea3d917b279d03eb4b6aa91cd
Dimitri :) <52245845+qizzle@users.noreply.github.com>
2025-09-23 22:09:48 +0200

    feat: update README, configure GitHub Actions for deployment, and fix PDF link path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://qizzle.github.io",
+  base: "/sweep-web",
   vite: {
     plugins: [tailwindcss()]
   }
